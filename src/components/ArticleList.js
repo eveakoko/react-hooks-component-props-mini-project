ee943f8c3defaa9a3699a54c91e3d9b27fdc98f9
eveakoko <you@example.com>
2024-05-07 16:49:42 +0300

    update react

---
+++ src/components/ArticleList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}) {
+    return (
+        <main>
+            {posts.map((post, index) => (
+                <Article key={index} {...post} />
+            ))}
+        </main>
+    );
+}
+export default ArticleList;
